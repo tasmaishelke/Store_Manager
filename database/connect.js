67e96274b25eaa0551b2d3e150e4f50ae2a77a79
Tasmai Shelke <tasmaishelke@gmail.com>
2024-02-15 20:22:04 +0530

    connect.js

--- database/connect.js
+++ database/connect.js
@@ -6,3 +6,16 @@ const connectDB = (url) =>
 }
 
 module.exports = connectDB
+
+// // Set up mongoose connection
+// const mongoose = require("mongoose");
+// mongoose.set("strictQuery", false);
+
+// const dev_db_url =
+//   "mongodb+srv://your_user_name:your_password@cluster0.lz91hw2.mongodb.net/local_library?retryWrites=true&w=majority";
+// const mongoDB = process.env.MONGODB_URI || dev_db_url;
+
+// main().catch((err) => console.log(err));
+// async function main() {
+//   await mongoose.connect(mongoDB);
+// }
